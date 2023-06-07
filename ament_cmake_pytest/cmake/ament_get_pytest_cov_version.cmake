02fd9c678d65c24b466c0978372bc2ad7d344924
Chris Lalancette <clalancette@gmail.com>
2023-06-07 14:13:51 +0000

    Imported upstream version '2.2.0' of 'upstream'

--- ament_cmake_pytest/cmake/ament_get_pytest_cov_version.cmake
+++ ament_cmake_pytest/cmake/ament_get_pytest_cov_version.cmake
@@ -17,9 +17,8 @@
 #
 # :param var: the output variable name
 # :type var: string
-# :param PYTHON_EXECUTABLE: absolute path to the Python interpreter to be used,
-#   default to the CMake variable with the same name returned by
-#   FindPythonInterp
+# :param PYTHON_EXECUTABLE: Python executable used to check the version
+#   It defaults to the CMake executable target Python3::Interpreter.
 # :type PYTHON_EXECUTABLE: string
 #
 # @public
@@ -32,10 +31,13 @@ function(ament_get_pytest_cov_version var)
   endif()
 
   if(NOT ARG_PYTHON_EXECUTABLE)
-    set(ARG_PYTHON_EXECUTABLE "${PYTHON_EXECUTABLE}")
+    set(ARG_PYTHON_EXECUTABLE Python3::Interpreter)
   endif()
 
-  set(cmd "${ARG_PYTHON_EXECUTABLE}" "-m" "pytest" "--version")
+  get_executable_path(python_interpreter "${ARG_PYTHON_EXECUTABLE}" CONFIGURE)
+
+  # Newer versions of pytest require providing '--version' twice to include plugin versions
+  set(cmd "${python_interpreter}" "-m" "pytest" "--version" "--version")
   execute_process(
     COMMAND ${cmd}
     RESULT_VARIABLE res
@@ -43,8 +45,8 @@ function(ament_get_pytest_cov_version var)
     ERROR_VARIABLE error)
   if(res EQUAL 0)
     # check if pytest-cov is in the list of plugins
-    # (actual output of the command is in ${error} and not ${output})
-    string(REGEX MATCH "pytest-cov-([0-9]\.[0-9]\.[0-9])" pytest_cov_full_version "${error}")
+    # (actual output of the command is in ${error} in pytest <7.0.0 and in ${output} in >=7.0.0)
+    string(REGEX MATCH "pytest-cov-([0-9]+\.[0-9]+\.[0-9]+)" pytest_cov_full_version "${output}${error}")
     if(pytest_cov_full_version)
       set(${var} ${CMAKE_MATCH_1} PARENT_SCOPE)
     else()
